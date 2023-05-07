6945cf95745970b3349c21a2a82d60831a0abd4f
Agryo Araujo <agryostallion@gmail.com>
2023-05-07 14:16:23 -0300

    Aula sobre o FATCH.

--- Site/js/main.js
+++ Site/js/main.js
@@ -1,1 +1,12 @@
-console.log('Sucesso!');
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+fetch(url) /* O Fetch é usado para chamar os comandos GET, PUT, etc. */
+    /* O Then serve como o Try, Catch, Finally */
+    .then((response) => response.json()) /* Significa o mesmo que chamar uma função que recebe a resposta e converte em JSON */
+    .then((jsonBody) => console.log(jsonBody)) /* Significa o mesmo que chamar outra função que recebe o JSON criado e exibe o JSON */
+    .catch((error) => console.error(error)) /* Significa o mesmo que chamar um função que recebe o ERRO e mostre esse erro. */
+
+const x = 10 + 10
+console.log(x)
