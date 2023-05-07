b0139a6d931515689337976091de21868bf48ee6
Agryo Araujo <agryostallion@gmail.com>
2023-05-07 15:32:49 -0300

    Manipulação de HTML com JS

--- Site/js/main.js
+++ Site/js/main.js
@@ -2,13 +2,43 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-/* O Fetch é usado para realizar a manipulação lógica do JSON que é retornado bruto e os THENs vão delimitando o que vou usar.  */
-fetch(url) /* O Fetch é usado para chamar os comandos GET, PUT, etc. do Browser, apartir da URL na variável. */
-    /* O Then serve como o Try, Catch, Finally */
-    .then((response) => response.json()) /* O primeiro Then, chama uma função que recebe a resposta da URL e converte em JSON */
-    .then((jsonBody) => jsonBody.results) /* O segundo Then, chama outra função que recebe o JSON criado e filtra o RESULTS desse JSON. */
-    .then((pokemonsList) => console.log(pokemonsList)) /* O último Then, chama outra função que exibe o JSON já filtrado. */
-    .catch((error) => console.error(error)) /* O Catch, chama um função que recebe o ERRO e mostra esse erro. */
+/* Está função faz a conversão do JSON para uma parte específica do meu HTML, nesse caso a "Li" da lista. */
+function convertPokemonToHtmlLi(pokemon) {
+    /* O RETURN faz o retorno do código HTML que vai ser adicionado ao INDEX da pagina principal. */
+    return `
+            <li class="pokemon">
+                <span class="number">#001</span>
+                <span class="name">${pokemon.name}</span>
 
-const x = 10 + 10
-console.log(x)
+                <div class="detail">
+                    <ol class="types">
+                        <li class="type">grass</li>
+                        <li class="type">poison</li>
+                    </ol>
+
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+                         alt="${pokemon.name}">
+                </div>
+            </li>
+            `
+}
+
+console.log(document.getElementById('pokemonsList'))
+
+/* O Fetch é usado para realizar a manipulação lógica do JSON que é retornado bruto e os THENs vão delimitando o que vou usar.  */
+//fetch(url) /* O Fetch é usado para chamar os comandos GET, PUT, etc. do Browser, apartir da URL na variável. */
+    /* O Then serve como o Try, Catch, Finally */
+//    .then((response) => response.json()) /* O primeiro Then, chama uma função que recebe a resposta da URL e converte em JSON */
+//    .then((jsonBody) => jsonBody.results) /* O segundo Then, chama outra função que recebe o JSON criado e filtra o RESULTS desse JSON. */
+    /* O último Then, chama outra função que exibe o JSON já filtrado. */
+//    .then((pokemonsList) => {
+        /* O For faz a seleção um por um dos pokemons. */
+//        for (let i = 0; i < pokemonsList.length; i++) {
+//            const pokemon = pokemonsList[i];
+//            console.log(convertPokemonToHtmlLi(pokemon));
+//        }
+//    })
+//    .catch((error) => console.error(error)) /* O Catch, chama um função que recebe o ERRO e mostra esse erro. */
+
+/*const x = 10 + 10
+console.log(x)*/
