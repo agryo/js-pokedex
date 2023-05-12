bca248581e5096518771afcd2e538ef74ea18ed4
Agryo Araujo <agryostallion@gmail.com>
2023-05-11 22:40:02 -0300

    Fanalizando

--- Site/js/main.js
+++ Site/js/main.js
@@ -1,3 +1,14 @@
+/* Essa variável pega o conteúdo do meu Index com a ID="pokemonsOlList" e guarda para alterações. */
+const pokemonsListHtml = document.getElementById('pokemonsOlList')
+
+/* Essa variável pega o conteúdo da pagination. */
+const carregarMaisButton = document.getElementById('carregarMaisButton')
+
+/* Aqui são os parametros usados na PokeAPI e o limite que eu quero. */
+const limiteTotal = 151
+const limit = 10
+let offset = 0;
+
 /* Está função faz a conversão do JSON para uma parte específica do meu HTML, nesse caso a "li" do HTML da lista. */
 function convertPokemonToHtmlLi(pokemon) {
     /* O RETURN faz o retorno do código HTML que vai ser adicionado ao INDEX da pagina principal. */
@@ -18,16 +29,32 @@ function convertPokemonToHtmlLi(pokemon) {
             `
 }
 
-/* Essa variável pega o conteúdo do meu Index com a ID="pokemonsOlList" e guarda para alterações. */
-const pokemonsListHtml = document.getElementById('pokemonsOlList')
-
 /* Está função chama o arquivo poke-api.js e usa as funções de lá. */
 /* Esse método getPokemons() é um dos métodos do arquivo poke-api.js */
 /* O ".then" segue na mesma linha ou não, pode ser delocado para abaixo para ficar mais legível. */
-pokeApi.getPokemons().then((pokemonsList = []) => {
-    /* Aqui ele faz a inclusão de toda a lista já convertida para o HTML do Index da página.
-       A variável pokemonsListHtml recebe a inclusão de toda a lista através do .map() que
-       serve como um For, só que chama o método que já transforma em HTML e junta com o Join('')
-     */
-    pokemonsListHtml.innerHTML += pokemonsList.map(convertPokemonToHtmlLi).join('')
+function carregarMaisPokemons(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemonsList = []) => {
+        /* Aqui ele faz a inclusão de toda a lista já convertida para o HTML do Index da página.
+           A variável pokemonsListHtml recebe a inclusão de toda a lista através do .map() que
+           serve como um For, só que chama o método que já transforma em HTML e junta com o Join('')
+         */
+        const novoHtml = pokemonsList.map(convertPokemonToHtmlLi).join('')
+        pokemonsListHtml.innerHTML += novoHtml
+    })    
+}
+
+carregarMaisPokemons(offset, limit)
+
+carregarMaisButton.addEventListener('click', () => {
+    offset += limit
+    const qtdPokemonsProximaPagina = offset + limit
+
+    if (qtdPokemonsProximaPagina >= limiteTotal) {
+        const novoLimit = limiteTotal - offset
+        carregarMaisPokemons(offset, novoLimit)
+
+        carregarMaisButton.parentElement.removeChild(carregarMaisButton)
+    } else {
+        carregarMaisPokemons(offset, limit)
+    }
 })
